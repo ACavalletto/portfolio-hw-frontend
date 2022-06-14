c5b3fbf4bb7a393f850e7ab221008f1d3bc10bb7
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-14 12:51:05 -0600

    updated footer with github and linkedin links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom'
 const Footer = (props) => {
+    const navStyle = {
+      display: 'flex',
+      justifyContent: 'space-around',
+      border: '3px solid black',
+      padding: '8px',
+      width: '90%',
+      margin: 'auto',
+    };
   return (
-    <div>Footer</div>
+    <footer>
+      <nav style={navStyle}>
+        <Link to="https://github.com/ACavalletto">
+          <div>GITHUB</div> 
+        </Link>
+        <Link to="www.linkedin.com/in/arnaud-cavalletto">
+          <div>LINKEDIN</div>
+        </Link>
+      </nav>
+    </footer>
   )
 }
 
